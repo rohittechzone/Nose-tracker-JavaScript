97a91a06ce22e2f04d3d3518a02682ac8425dca9
Rohit <63205012+rohittechzone@users.noreply.github.com>
2020-11-04 18:31:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,9 @@
-function preload() {
-  
-}
-
 function setup() {
 	canvas = createCanvas(800,400);
 	video = createCapture(VIDEO);
-	video.size(800,400);
-
+	video.hide();
+	//video.size(800,400);
+    disVideo = video;
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
 
@@ -18,14 +15,16 @@ function modelLoaded() {
 
   function gotPoses(results)
   {
+	background("white");
+	image(disVideo,0,0);
 	if(results.length > 0)
 	{
 	  noseX = results[0].pose.nose.x;
 	  noseY = results[0].pose.nose.y;
+	  fill("#0000");
+	  strokeWeight(5);
+	  stroke("blue");
+	  rect(noseX-10, noseY-5, 25, 25);
       console.log(noseY);
 	}
   }
-   
-function draw() {
-  
-}
